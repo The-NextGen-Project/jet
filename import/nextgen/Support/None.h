23f830f1c5b384cf1dec8cc541a85b39942b7839
aboss123 <ashishbailkeri123@gmail.com>
2021-06-12 19:26:58 -0400

    :penguin: Cross-platform shifting

--- import/nextgen/Support/None.h
+++ import/nextgen/Support/None.h
@@ -15,6 +15,12 @@ namespace nextgen { // Putting this in the global namespace
 # define match(...) switch (pair(__VA_ARGS__))
 # define group(...) case pair(__VA_ARGS__)
 
+#   ifdef BIT64
+#       define SHIFT_PAIR 16
+#   else
+#       define SHIFT_PAIR 8
+#   endif
+
   template <typename Enum>
   constexpr int pair(Enum k1) { // STD NAMING
     return k1;
@@ -22,29 +28,17 @@ namespace nextgen { // Putting this in the global namespace
 
   template<typename Enum1, typename Enum2>
   constexpr int pair(Enum1 k1, Enum2 k2) {
-# ifdef BIT32
-    return (((size_t)k1 + (size_t)k2) << 8) | (size_t)k2;
-# endif
+    return ((k1 + k2) << SHIFT_PAIR) | (size_t)k2;
   }
 
   template<typename Enum1, typename Enum2, typename Enum3>
   constexpr int pair(Enum1 k1, Enum2 k2, Enum3 k3) {
-# ifdef BIT32
-    return ((k1 + k2 + k3) << 8) | (k2 * k3);
-# endif
-# ifdef BIT64
-    return ((k1 + k2 + k3) << 16) | (k2 * k3);
-# endif
+    return ((k1 + k2 + k3) << SHIFT_PAIR) | (k2 * k3);
   }
 
   template<typename Enum1, typename Enum2, typename Enum3, typename Enum4>
   constexpr int pair(Enum1 k1, Enum2 k2, Enum3 k3, Enum4 k4) {
-# ifdef BIT32
-    return ((k1 + k2 + k3 + k4) << 8) | (k2 * k3 * k4);
-# endif
-# ifdef BIT64
-    return ((k1 + k2 + k3 + k4) << 16) | (k2 * k3 * k4);
-# endif
+    return ((k1 + k2 + k3 + k4) << SHIFT_PAIR) | (k2 * k3 * k4);
   }
 
 
